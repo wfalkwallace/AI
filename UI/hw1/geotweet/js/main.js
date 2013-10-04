7a2884058244ed1f0feef414bf2f4373ed1abefd
William Falk-Wallace <wfalkwallace@gmail.com>
2013-10-03 20:04:54 -0400

    adds credentials and cred-print

--- UI/hw1/geotweet/js/main.js
+++ UI/hw1/geotweet/js/main.js
@@ -4,3 +4,20 @@ $('.input-daterange').datepicker({
     todayBtn: "linked",
     autoclose: true
 });
+
+var credentials = 
+{
+consumer_key: 'sZU3etBMqj7BeHEetZO6w'
+consumer_secret: 'rWxPl3oWesD31Gg56SLsXGAcGkyL9ux0LhlAncGK8A'
+access_token: '1932368732-nOVLlgcqp5ATBG3VAZrwLopVsuEj3BFysho4cqR'
+access_token_secret: 'qThVWaxB7llq1LrS4Ee3mM5iK18n410Jxbzoc4Q2aUo'
+};
+
+$('#info-header').append(
+    '<h3>Using Credentials:</h3>' +
+    '<ul><li><strong>Consumer Key: </strong>' + credentials[consumer_key] + '</li>' +
+    '<li><strong>Consumer Secret: </strong>' + credentials[consumer_secret] + '</li>' +
+    '<li><strong>Access Token: </strong>' + credentials[access_token] + '</li>' +
+    '<li><strong>Access Token Secret: </strong>' + credentials[access_token_secret] + '</li></ul>'
+                            
+);
