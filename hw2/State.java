eaa44b72547e6eefa6269f09cf7692895aa49b9b
William Falk-Wallace <wfalkwallace@gmail.com>
2013-10-21 23:44:31 -0400

    state _should_ be done

--- hw2/State.java
+++ hw2/State.java
@@ -19,21 +19,56 @@ public class State {
 //	+ (plus) Player on goal 
 //	$ (dollar) Box on floor
 //	* (asterisk) Box on goal
-//
-//	 <0 - Wall
-//	  0 - empty space
-//	  1 - empty goal
-//	  2 - player
-//	  3 - player on goal
-//	  4 - box
-//	  5 - box on goal
 
-	private int[][] map;
-	private int width;
-	private int height;
+	private char[][] map;
 
-	public State (int[][] map) {
+	public State (char[][] map) {
 		this.map = map;
 	}
 	
+	public char[][] getMap() {
+		return map;
+	}
+	
+	public void printState() {
+		for(char[] row : map){
+			for(char c : row){
+				System.out.print(c);
+			}
+			System.out.println();
+		}
+	}
+	
+	public int[] getLocation() {
+		for(int i = 0; i < map.length; i++) {
+			for(int j = 0; j < map[i].length; i++) {
+				if(map[i][j] == '@' || map[i][j] == '+')
+					return {i, j};
+			}
+		}
+	}
+	
+	public char[] getValidMoves() {
+		int[] loc = getLocation();
+		char[] moves = new char[4];
+		if(map[loc[0] + 1][loc[1]] == '.' || 
+		   map[loc[0] + 1][loc[1]] == '$' ||
+		   map[loc[0] + 1][loc[1]] == '*') 
+			moves[0] = 'u';
+		else if(map[loc[0] - 1][loc[1]] == '.' || 
+		        map[loc[0] - 1][loc[1]] == '$' ||
+		        map[loc[0] - 1][loc[1]] == '*') 
+			moves[1] = 'd';
+		else if(map[loc[0]][loc[1] + 1] == '.' || 
+		        map[loc[0]][loc[1] + 1] == '$' ||
+		        map[loc[0]][loc[1] + 1] == '*') 
+			moves[2] = 'r';
+		else if(map[loc[0]][loc[1] - 1] == '.' || 
+		        map[loc[0]][loc[1] - 1] == '$' ||
+		        map[loc[0]][loc[1] - 1] == '*') 
+			moves[3] = 'l';
+		
+		return moves;
+	}
+
 }
