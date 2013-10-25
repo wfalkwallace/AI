cd50a1869e4d07408248a30112f1cb8b4a22a551
William Falk-Wallace <wfalkwallace@gmail.com>
2013-10-25 12:12:35 -0400

    adds statestring and overrides equals and hash to utilize it

--- hw2/State.java
+++ hw2/State.java
@@ -9,32 +9,37 @@
  */
 public class State {
 
-	
-	
-//	# (hash) Wall
-//	
-//	. (period) Empty goal
-//	@ (at) Player on floor
-//	+ (plus) Player on goal 
-//	$ (dollar) Box on floor
-//	* (asterisk) Box on goal
 
-	private char[][] map;
+
+	//	# (hash) Wall
+	//	
+	//	. (period) Empty goal
+	//	@ (at) Player on floor
+	//	+ (plus) Player on goal 
+	//	$ (dollar) Box on floor
+	//	* (asterisk) Box on goal
+
+	private String statestring;
+	private char[][] level;
 	private int x;
 	private int y;
 
 	public State (char[][] map, int x, int y) {
-		this.map = map;
+		this.level = map;
 	}
-	
-	public char[][] getMap() {
-		return map;
+
+	public char[][] getState() {
+		return level;
 	}
-	
-	public int[] getPlayer() {
-		return new int[] {x, y};
+
+	public int getX() {
+		return x;
 	}
-	
+
+	public int getY() {
+		return y;
+	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -44,33 +49,67 @@ public class State {
 	}
 
 	public void printState() {
-		for(char[] row : map){
+		for(char[] row : level){
 			for(char c : row){
 				System.out.print(c);
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
-	
+
+	public String getStateString() {
+		if(statestring != null)
+			return statestring;
+		statestring = "";
+		for(char[] row : level){
+			for(char c : row){
+				statestring += c;
+			}
+		}
+		return statestring;
+	}
+
+
+    public int hashCode() {
+        return statestring.hashCode();
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (!(obj instanceof State))
+            return false;
+        return ( ((State) obj).getStateString() == this.getStateString() ) ? true : false;
+    }
+
+
+
+
+
+
+
+
+
 	public char[] getValidMoves() {
 		char[] moves = new char[4];
-		if(map[x + 1][y] == '.' || 
-		   map[x + 1][y] == '$' ||
-		   map[x + 1][y] == '*') 
+		if(level[x + 1][y] == '.' || 
+				level[x + 1][y] == '$' ||
+				level[x + 1][y] == '*') 
 			moves[0] = 'u';
-		else if(map[x - 1][y] == '.' || 
-		        map[x - 1][y] == '$' ||
-		        map[x - 1][y] == '*') 
+		else if(level[x - 1][y] == '.' || 
+				level[x - 1][y] == '$' ||
+				level[x - 1][y] == '*') 
 			moves[1] = 'd';
-		else if(map[x][y + 1] == '.' || 
-		        map[x][y + 1] == '$' ||
-		        map[x][y + 1] == '*') 
+		else if(level[x][y + 1] == '.' || 
+				level[x][y + 1] == '$' ||
+				level[x][y + 1] == '*') 
 			moves[2] = 'r';
-		else if(map[x][y - 1] == '.' || 
-		        map[x][y - 1] == '$' ||
-		        map[x][y - 1] == '*') 
+		else if(level[x][y - 1] == '.' || 
+				level[x][y - 1] == '$' ||
+				level[x][y - 1] == '*') 
 			moves[3] = 'l';
-		
+
 		return moves;
 	}
 
