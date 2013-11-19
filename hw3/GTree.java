a815714d7b88fa3a66abbbe1d45ef878b7635c7e
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-19 04:17:01 -0500

    adds soko DFS template

--- hw3/GTree.java
+++ hw3/GTree.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -26,24 +28,6 @@ public class GTree {
 		return root;
 	}
 
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	private String solution(GState solution, long start) {
 		JUSTKEEPSWIMMING = false;
 		//give in millis
@@ -56,40 +40,44 @@ public class GTree {
 		return fail;
 	}
 
-	//BFS
-	public String[] BFS() {
+	//DFS
+	public String[] DFS() {
 		JUSTKEEPSWIMMING = true;
 		//data and results
 		long start = System.nanoTime();
 		int rep = 0;
 		int gen = 1;
+
 		//here we go
-		GState node = root;
+		State node = root;
 		HashSet<String> explored = new HashSet<String>();
-		Queue<State> frontier = new LinkedList<State>();
-		frontier.add(node);
-		if(node.isGoal())
-			return solution(node, gen, rep, frontier.size(), explored.size(), start);
+		Stack<State> frontier = new Stack<State>();
+		
+		frontier.push(node);
 		while(JUSTKEEPSWIMMING) {
 			if(frontier.peek() == null)
 				return failure();
-			node = frontier.poll();
-			explored.add(node.getStateString());
-			for(char c : node.getValidMoves()) {
-				State child = new State(node, c);
-				gen++;
-				if( !explored.contains(child.getStateString()) && !frontier.contains(child) ) {
-					if(child.isGoal())
-						return solution(child, gen, rep, frontier.size(), explored.size(), start);
-					frontier.add(child);
+			node = frontier.pop();
+			if( !explored.contains(node.getStateString()) ) {
+				explored.add(node.getStateString());
+				if(node.isGoal())
+					return solution(node, gen, rep, frontier.size(), explored.size(), start);
+				for(char c : node.getValidMoves()) {
+					State child = new State(node, c);
+					gen++;
+					if( !explored.contains(child.getStateString()) ) {
+						frontier.add(child);
+					}
+					else
+						rep++;
 				}
-				else if(explored.contains(child.getStateString()))
-					rep++;
 			}
+			else
+				rep++;
 		}
 		return new String[0];
 	}
-	//ENDBFS
+	//ENDDFS
 
 
 
