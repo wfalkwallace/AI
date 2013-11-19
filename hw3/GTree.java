a1293b777244cf96251d352cb01ce87e21d725db
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-19 15:27:56 -0500

    the game runs

--- hw3/GTree.java
+++ hw3/GTree.java
@@ -1,11 +1,7 @@
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Hashtable;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Random;
-import java.util.Stack;
+import java.util.Scanner;
 
 /**
  * 
@@ -20,50 +16,82 @@ public class GTree {
 	private static Random random = new Random();
 	private static final int DEPTH_LIMIT = 5;
 	private Hashtable<String, Integer> explored = new Hashtable<String, Integer>();
-
 	private GState current;
 	private int timeout;
+	private long start;
+	private Scanner input;
+
 	public GTree(int boardsize, int chainlength, int timeout){
 		current = new GState(boardsize, chainlength);
 		this.timeout = timeout;
+		//user input
+		input = new Scanner(System.in);
 	}
-	
-	public void game(int p1, int p2) {
-		
-		
+
+	public char game(int p1, int p2) {
+		while(true) {
+			start = System.nanoTime();
+			move(p1, start);
+			current.printState();
+			if(current.isWin())
+				//TODO WIN
+				return current.getParent().getPlayer();
+			else if(current.isDraw())
+				//TODO DRAW
+				return '0';
+			start = System.nanoTime();
+			move(p2, start);
+			current.printState();
+			if(current.isWin())
+				//TODO WIN
+				return current.getParent().getPlayer();
+			else if(current.isDraw())
+				//TODO DRAW
+				return '0';
+		}
 	}
-	
-	
-	private void move (int mode) {
+
+
+	private void move (int mode, long start) {
 		switch(mode) {
 		case 0:
 			randomMove();
 			break;
 		case 1:
-			promptMove();
+			promptMove(start);
 			break;
 		case 2:
-			abSearch();
+			abSearch(start);
 			break;
 		}		
 	}
-	
+
 	private void randomMove() {
 		ArrayList<int[]> moves = current.getActions();
 		int[] move = moves.get(random.nextInt(moves.size()));
 		current = current.getResult(move[0], move[1]);
 	}
-	
-	
-	
-	
-	
-	private int[] abSearch() {
+
+	private void promptMove(long start) {
+		//		if((System.nanoTime() - start) / 1000000000.0 < timeout) {
+		System.out.println("Row:");
+		int row = input.nextInt();
+		System.out.println("Column:");
+		int col = input.nextInt();
+		current = current.getResult(row, col);
+		//		}
+	}
+
+	public void closeScanner() {
+		input.close();
+	}
+
+	private int[] abSearch(long start) {
 		int v = maxValue(current, -1000000000, 1000000000, 0);
 		//TODO: which one to get?
 		return current.getActions().get(0);
 	}
-	
+
 	private int maxValue(GState state, int alpha, int beta, int depth) {
 		if(cutoff(state, depth))
 			return utility(state);
@@ -76,7 +104,7 @@ public class GTree {
 		}
 		return v;
 	}
-	
+
 	private int minValue(GState state, int alpha, int beta, int depth) {
 		if(cutoff(state, depth))
 			return utility(state);
@@ -89,87 +117,29 @@ public class GTree {
 		}
 		return v;
 	}
-	
+
 	private int min(int a, int b) {
 		return (a < b) ? a : b;
 	}
-	
+
 	private int max(int a, int b) {
 		return (a > b) ? a : b;
 	}
-	
+
 	private boolean cutoff(GState state, int depth) {
 		return depth == DEPTH_LIMIT;
 	}
-	
+
 	//TODO Utility
 	private int utility(GState state) {
 		int u = 0;
-		
-		
-		
+
+
+
 		return u;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	private String solution(GState solution, long start) {
-		JUSTKEEPSWIMMING = false;
-		//give in millis
-		double time = (System.nanoTime() - start) / 1000000000.0;
-		return "time: " + time;
-	}
 
-	private String[] failure() {
-		String[] fail = {"Search Completed and No Solution Found"};
-		return fail;
-	}
 
-	//DFS
-	public String[] DFS() {
-		JUSTKEEPSWIMMING = true;
-		//data and results
-		long start = System.nanoTime();
-		int rep = 0;
-		int gen = 1;
-
-		//here we go
-		State node = root;
-		HashSet<String> explored = new HashSet<String>();
-		Stack<State> frontier = new Stack<State>();
-		
-		frontier.push(node);
-		while(JUSTKEEPSWIMMING) {
-			if(frontier.peek() == null)
-				return failure();
-			node = frontier.pop();
-			if( !explored.contains(node.getStateString()) ) {
-				explored.add(node.getStateString());
-				if(node.isGoal())
-					return solution(node, gen, rep, frontier.size(), explored.size(), start);
-				for(char c : node.getValidMoves()) {
-					State child = new State(node, c);
-					gen++;
-					if( !explored.contains(child.getStateString()) ) {
-						frontier.add(child);
-					}
-					else
-						rep++;
-				}
-			}
-			else
-				rep++;
-		}
-		return new String[0];
-	}
-	//ENDDFS
 
 
 
