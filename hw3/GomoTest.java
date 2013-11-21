69dc97b09028c9a1d652742c130857761d2ea75d
William Falk-Wallace <wfalkwallace@gmail.com>
2013-11-20 23:03:13 -0500

    minor updates?

--- hw3/GomoTest.java
+++ hw3/GomoTest.java
@@ -36,7 +36,7 @@ public class GomoTest {
 		}
 		
 		GTree tree = new GTree(boardsize, chainlength, timeout);
-		System.out.println("Player " + tree.game(1, 0) + " WINS!");
+		System.out.println("Player " + tree.game(0, 0) + " WINS!");
 		tree.closeScanner();
 		
 		
