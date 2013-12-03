cbed51929c03393dca3222d7d3b22c2d2c61a096
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-03 12:24:46 -0500

    updates entail to set up the kb and take for/back string input

--- hw4/Entail.java
+++ hw4/Entail.java
@@ -18,55 +18,37 @@ public class Entail {
 
 
 
-		int mode;
+		String mode;
 		String kbpath;
 		String query;
 
 		if(args.length < 3){
 			//user input
 			Scanner input = new Scanner(System.in);
-			System.out.println("Enter a mode:");
-			System.out.println("1) Forward");
-			System.out.println("2) Backward");
-			mode = input.nextInt();
+			System.out.println("Enter a mode [forward/backward]:");
+			mode = input.nextLine();
 			System.out.println("Enter a knowledge base file [<filepath>]:");
 			kbpath = input.nextLine();
 			System.out.println("Enter a query symbol to be entailed:");
 			query = input.nextLine();
 		}
 		else {
-			mode = Integer.parseInt(args[0]);
+			mode = args[0];
 			kbpath = args[1];
 			query = args[2];
 		}
 
 		File kbfile = new File(kbpath);
-		KBLoader kbl = new KBLoader(kbfile);		
-
-		String searchstring;
-		switch(mode) {
-		case 1:
-			//FORWARD
-			System.out.println("Running Forward Chaining on,");
-			break;
-		case 2:
-			//BACKWARD
-			System.out.println("Running Backward Chaining on,");
-			break;
-
+		KB kb = new KB(kbfile);
+		
+		if ( mode.equals("forward") ) {
+			kb.fc(query);
 		}
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		else if ( mode.equals("backward") ) {
+			kb.bc(query);
+		}
+		else
+			System.out.println("That's not a valid mode.");
 
 
 	}
