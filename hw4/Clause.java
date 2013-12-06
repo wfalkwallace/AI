4a8d00e3199522d2dbd104473a1f2a8a7af6f88a
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-06 03:58:47 -0500

    some fleshed out clause constructor

--- hw4/Clause.java
+++ hw4/Clause.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -8,7 +10,24 @@
  */
 public class Clause {
 
-	
-	
-	
+	private ArrayList<String> premise;
+	private String conclusion;
+	private int count;
+
+	Clause(String s) {
+		String splitclause[] = s.split("=>");
+		conclusion = splitclause[1];
+		
+		String splitpremise[] = s.split(" ");
+		for ( String sym : splitpremise ){
+			if ( sym.contains("~") )
+				sym.replace("~", "");
+			premise.add(sym);
+		}
+		
+		count = premise.size();
+		
+	}
+
+
 }
