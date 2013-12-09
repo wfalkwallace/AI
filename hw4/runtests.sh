141d02cf82c2d5d00b5723f125f38b62529f5544
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-08 23:08:44 -0500

    updates runtest shell formatting and log sample

--- hw4/runtests.sh
+++ hw4/runtests.sh
@@ -4,8 +4,36 @@
 # Description = Script to run AI HW2 Tests
 
 make
-java Entail "forward" "kbs/0.txt" "Q" > log.txt
-java Entail "backward" "kbs/0.txt" "Q" >> log.txt
-java Entail "forward" "kbs/1.txt" "PIGN" >> log.txt
-java Entail "backward" "kbs/1.txt" "PIGN" >> log.txt
+
+
+printf "====================\n" > log.txt
+
+
+printf "Query: Q \nKB: \n" >> log.txt
+cat kbs/0.txt >> log.txt
+printf "\n\nForward Chaining:\n" >> log.txt
+java Entail forward kbs/0.txt Q >> log.txt
+printf "\n\nBackward Chaining:\n" >> log.txt
+#java Entail "backward" "kbs/0.txt" "Q" >> log.txt
+printf "====================\n" >> log.txt
+
+
+printf "Query: PIGN \nKB: \n" >> log.txt
+cat kbs/1.txt >> log.txt
+printf "\n\nForward Chaining:\n" >> log.txt
+java Entail forward kbs/1.txt PIGN >> log.txt
+printf "\n\nBackward Chaining:\n" >> log.txt
+#java Entail "backward" "kbs/1.txt" "PIGN" >> log.txt
+printf "====================\n" >> log.txt
+
+
+printf "Query: Q \nKB: \n" >> log.txt
+cat kbs/2.txt >> log.txt
+printf "\n\nForward Chaining:\n" >> log.txt
+java Entail forward kbs/2.txt Q >> log.txt
+printf "\n\nBackward Chaining:\n" >> log.txt
+#java Entail backward kbs/2.txt Q >> log.txt
+printf "====================\n" >> log.txt
+
+
 make clean
